829a16465d9affec70aecd846fb19b91e9776096
Princy Patel <kapatelprincy@gmail.com>
2024-03-13 00:12:23 +0530

    add home page

---
+++ src/Components/Home.jsx
@@ -0,0 +1,26 @@
+import classes from "../styles/home.module.css";
+
+function Home() {
+  return (
+    <div
+      className={`h-screen w-full relative bg-no-repeat ${classes.home_image}`}
+    >
+      <div className="absolute bottom-0 px-4 py-3 bg-zinc-900/60 w-full h-screen flex flex-col gap-20 justify-center items-center">
+        <div>
+          <h1 className="text-white text-[8vw] font-['Roboto_Condensed'] leading-none tracking-tight text-center">
+            Furniture, decore,
+          </h1>
+          <h1 className="text-white text-[8vw] font-['Roboto_Condensed'] leading-none tracking-tight text-center">
+            and beyond
+          </h1>
+        </div>
+
+        <div className="border-b-2">
+           <a href="#">SHOP NOW</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
